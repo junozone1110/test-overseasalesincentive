a1a1ad0a52c82c09dfb78159b5317bb79c426a4e
zone <zone+github@giftee.co>
2025-09-25 00:01:35 +0900

    Fix: カスタムUserモデルの再帰エラーを解消
    - is_staff/is_superuserプロパティの無限再帰問題を修正
    - プロパティ定義を削除し、save()メソッドでシンプルに処理
    - Docker環境でのユーザー作成・パスワード変更が正常動作
    - 管理者権限の自動付与機能を維持
    
    Changes:
    - accounts/models.py: プロパティ削除、save()メソッド簡素化
    - マイグレーション追加: is_staff/is_superuserフィールド管理

--- accounts/models.py
+++ accounts/models.py
@@ -21,26 +21,7 @@ class User(AbstractUser):
     def save(self, *args, **kwargs):
         # is_adminがTrueの場合、is_staffとis_superuserも自動的にTrueに設定
         if self.is_admin:
-            super(User, self).__setattr__('is_staff', True)
-            super(User, self).__setattr__('is_superuser', True)
+            # 親クラスのフィールドに直接設定
+            super(User, self).__dict__['is_staff'] = True
+            super(User, self).__dict__['is_superuser'] = True
         super().save(*args, **kwargs)
-
-    @property
-    def is_staff(self):
-        """管理者権限があればDjango管理画面にアクセス可能"""
-        return self.is_admin or super().is_staff
-
-    @is_staff.setter
-    def is_staff(self, value):
-        """is_staffの設定"""
-        super(User, self).__setattr__('is_staff', value)
-
-    @property
-    def is_superuser(self):
-        """管理者権限があればスーパーユーザー扱い"""
-        return self.is_admin or super().is_superuser
-
-    @is_superuser.setter
-    def is_superuser(self, value):
-        """is_superuserの設定"""
-        super(User, self).__setattr__('is_superuser', value)
